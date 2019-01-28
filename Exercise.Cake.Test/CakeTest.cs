a7c35a82ac8c7ab8ef8a835bb004b0dc69eee9b6
Mikus Miroslav <miroslav.mikus@outlook.com>
2019-01-28 05:16:06 +0100

    rename unit-test to cake test

--- Exercise.Cake.Test/CakeTest.cs
+++ Exercise.Cake.Test/CakeTest.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Exercise.Cake.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class CakeTest
     {
         [TestMethod]
         public void TestSum()
